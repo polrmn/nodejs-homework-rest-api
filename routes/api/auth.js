8ae570a05c9d8e8e46f61f00e11dc699ea17ece7
polrmn <polrmn@gmail.com>
2023-04-06 17:51:33 +0300

    test

---
+++ routes/api/auth.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const HttpError = require("../../helpers/HttpError");
+
+const router = express.Router()
+
+const Joi = require("joi");
+
+
+
+module.exports = router
